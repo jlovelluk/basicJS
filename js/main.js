c9e2d7f7e7cda7ccb7b40c50936759f0cd5a7d00
Jason Lovell <jasonlovelluk@outlook.com>
2019-10-10 16:45:34 +0100

    up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+//Enter action for sum output
+
 function myFunction(){
+
+    //Set variables
+
+    //Collect input values
     
     var fNum = parseInt(document.getElementById("fNumber1").value);
     
@@ -6,19 +12,55 @@ function myFunction(){
 
     var tNum = parseInt(document.getElementById("fNumber3").value);
 
+    //Result variables
 
-    
+    var textMsg = document.getElementById("textMsg");
+
+    var outPut = document.getElementById("outPut");
+
+    //Mathmatics variable
+
     var x = fNum + sNum + tNum;
 
-    if (x % 2 === 0) {
+    //Show sum result and whether it is odd or even
 
-        document.getElementById("textMsg").innerHTML="This number is even";
-        document.getElementById("outPut").innerHTML=x;
+    if ( isNaN(x)) {
+
+        textMsg.innerHTML="You must only use numbers";
+        outPut.innerHTML=""; 
+
+    } else if (x % 2 === 0) {
+
+        //Even number output
+
+        textMsg.innerHTML="This number is even";
+        outPut.innerHTML=x;
 
     } else {
 
-        document.getElementById("textMsg").innerHTML="This number is an odd number";
-        document.getElementById("outPut").innerHTML=x;
+        //Odd number output
 
+        textMsg.innerHTML="This number is odd";
+        outPut.innerHTML=x;
+ 
     }
+}
+
+// Reset button action
+
+function myReset(){
+
+    var resultMsg = "Result Here!";
+
+    // Reset three input values to zero
+
+    document.getElementById("fNumber1").value = "";
+    document.getElementById("fNumber2").value = "";
+    document.getElementById("fNumber3").value = "";
+
+    // Reset result and odd or even message
+
+    textMsg.innerHTML = "";
+    outPut.innerHTML= resultMsg;
+
 }
